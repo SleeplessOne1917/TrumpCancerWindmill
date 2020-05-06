fe5efe41998795b2f028bd4995b8352c2fb3ffca
abias <kekkekK3KK3K>
2020-05-06 02:59:15 -0400

    Setup C# project

---
+++ Source/Trump Cancer Windmill/CompPowerPlantWindPatches.cs
@@ -0,0 +1,14 @@
+﻿using RimWorld;
+using Verse;
+
+namespace Trump_Cancer_Windmill
+{
+    [StaticConstructorOnStartup]
+    public static class CompPowerPlantWindPatches
+    {
+        static CompPowerPlantWindPatches()
+        {
+            Log.Message("Hello World!");
+        }
+    }
+}
