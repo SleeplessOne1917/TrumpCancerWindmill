09a003d616bdf86cb635639f12d3a40d622ff4df
abias <kekkekK3KK3K>
2020-05-06 04:05:07 -0400

    Add postfix method to wind turbine tick computation

---
+++ Source/Trump Cancer Windmill/CompPowerPlantWind_CompTick_Patch.cs
@@ -0,0 +1,25 @@
+﻿using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace Trump_Cancer_Windmill
+{
+    [StaticConstructorOnStartup]
+    public static class CompPowerPlantWind_CompTick_Patch
+    {
+        static CompPowerPlantWind_CompTick_Patch()
+        {
+            var harmony = new Harmony("abias1122.TrumpWindmillSound");
+
+            harmony.Patch(
+                AccessTools.Method(typeof(CompPowerPlantWind), nameof(CompPowerPlantWind.CompTick)),
+                null,
+                new HarmonyMethod(typeof(CompPowerPlantWind_CompTick_Patch), nameof(PostfixCompTick)));
+        }
+
+        static void PostfixCompTick()
+        {
+            Log.Message("Logging wind turbine tick");
+        }
+    }
+}
