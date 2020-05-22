5913eb3b3f9724691e48238b548b32a6fcfbd6ae
abias <kekkekK3KK3K>
2020-05-22 19:34:14 -0400

    Add settings for cancer radius and cause chance

--- Source/Trump Cancer Windmill/CancerConstants.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Trump_Cancer_Windmill
-{
-    public class CancerConstants
-    {
-        public const float CANCER_RADIUS = 20;
-        public const float CANCER_CHANCE = 0.00001f;
-    }
-}
